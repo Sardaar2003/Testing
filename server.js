b9fa4372b4edee10b8ff54c3d5933e77d5dd20f7
Sardaar2003 <103274914+Sardaar2003@users.noreply.github.com>
2024-04-09 20:55:01 +0530

    Updating

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
