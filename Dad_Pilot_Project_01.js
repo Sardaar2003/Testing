249c500d0a5a875bd249e789e8a758d7032eee52
Sardaar2003 <103274914+Sardaar2003@users.noreply.github.com>
2024-04-09 23:00:04 +0530

    Update Dad_Pilot_Project_01.js

--- Dad_Pilot_Project_01.js
+++ Dad_Pilot_Project_01.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "rewardscenter.club",
+    origin: "rewardscenter.club/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
