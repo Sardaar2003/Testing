2a9c39bf4de6749b2b2336bcae70d76025e0549a
Sardaar2003 <103274914+Sardaar2003@users.noreply.github.com>
2024-04-10 20:20:33 +0530

    Update Dad_Pilot_Project_01.js

--- Dad_Pilot_Project_01.js
+++ Dad_Pilot_Project_01.js
@@ -3,19 +3,31 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+
+const allowedOrigins = ["rewardscenter.club/"];
+
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://rewardscenter.club/",
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Origin not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 const port = 8080;
+
 app.listen(port, () => {
   console.log(__dirname);
   console.log(`App is Listening on ${port}`);
 });
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
